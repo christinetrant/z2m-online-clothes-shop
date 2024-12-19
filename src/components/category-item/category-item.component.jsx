c0369e28822d6cbf7dc3727472d15854bcbc7579
Christine Trant <christine@umidigital.co.uk>
2024-12-19 08:54:00 +0000

    split out categories container and category items into their own components

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./category-item.styles.scss";
+
+const CategoryItem = ({category}) => {
+    const {id, title, imageUrl} = category;
+    return (
+        <div className="category-container">
+            <div className="category">
+                <div
+                    className="background-image"
+                    style={{
+                        backgroundImage: `url(${imageUrl})`,
+                    }}
+                ></div>
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    );
+};
+
+export default CategoryItem;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./directory.style.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+const Directory = ({categories}) => {
+    return (
+        <div className="App">
+            <div className="categories-container">
+                {categories.map((category) => (
+                    <CategoryItem key={category.id} category={category} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Directory;
