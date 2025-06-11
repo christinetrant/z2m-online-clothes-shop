75afa10cb0f534ed9a7155832e2c98c98bdaf1a4
Christine <christinetrant@gmail.com>
2025-06-11 16:46:28 +0100

    Install new library 'reslect' to memoize category state

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,9 +1,28 @@
+import { createSelector } from "reselect";
 // export const selectCategoriesMap = (state) => state.categories.categories;
-export const selectCategoriesMap = (state) => {
-	const categoriesMap = state.categories.categories.reduce((acc, category) => {
+// We'll use memoization here using reselect library to only re-render if the state changes
+
+// Get the slice of state we want to use
+const selectCategoryReducer = (state) => state.categories;
+
+// createSelector takes in an array of input selectors and a function that returns the output selector
+export const selectCategories = createSelector([selectCategoryReducer], (categoriesSlice) => categoriesSlice.categories);
+
+// export const selectCategoriesMap = (state) => {
+// 	const categoriesMap = state.categories.categories.reduce((acc, category) => {
+// 		const { title, items } = category;
+// 		acc[title.toLowerCase()] = items;
+// 		return acc;
+// 	}, {});
+// 	return categoriesMap;
+// };
+
+// Use memoization to only re-render if the state changes
+export const selectCategoriesMap = createSelector([selectCategories], (categories) =>
+	categories.reduce((acc, category) => {
+		console.log("selector fired");
 		const { title, items } = category;
 		acc[title.toLowerCase()] = items;
 		return acc;
-	}, {});
-	return categoriesMap;
-};
+	}, {})
+);
--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,22 @@ import { logger } from "redux-logger";
 // Root Reducer contains all the reducers needed to create the store
 import { rootReducer } from "./root-reducer";
 
+// Below is an example of creating our own middleware
+// Uses a currying function and will re render a component when the store is updated and when next is called. So will update synchronously unlike the logger middleware which bundles all console logs together so will be out of order
+// const loggerMiddleWare = (store) => (next) => (action) => {
+// 	if (!action.type) {
+// 		return next(action);
+// 	}
+// 	console.log("type: ", action.type);
+// 	console.log("payload: ", action.payload);
+// 	console.log("currentState: ", store.getState());
+
+// 	next(action);
+
+// 	console.log("next state: ", store.getState());
+// };
+// const middleWares = [loggerMiddleWare];
+
 // Library helpers that run the items in the array before an action hits the reducer
 const middleWares = [logger];
 // In order for the middleware to work, we need to apply the middleware
