fe580b5accc45a371254f061b61b7d24e6fccefc
Christine <christinetrant@gmail.com>
2025-06-18 14:56:40 +0100

    setCurrentUser action is created by redux-tollkit in user.reducer

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 import { useEffect } from "react";
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "./utils/firebase/firebase.utils";
-import { setCurrentUser } from "./store/user/user.action";
+// import { setCurrentUser } from "./store/user/user.action";
+import { setCurrentUser } from "./store/user/user.reducer";
 import { useDispatch } from "react-redux";
 
 const App = () => {
